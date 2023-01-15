4a877af4ed2e2325ae153c33710ab479c722729c
Tom Silver <tomssilver@gmail.com>
2023-01-15 10:53:11 -0500

    actually add files

---
+++ llmclone/utils.py
@@ -0,0 +1,11 @@
+"""Utilities."""
+
+import functools
+import subprocess
+
+
+@functools.lru_cache(maxsize=None)
+def get_git_commit_hash() -> str:
+    """Return the hash of the current git commit."""
+    out = subprocess.check_output(["git", "rev-parse", "HEAD"])
+    return out.decode("ascii").strip()
